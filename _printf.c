3286c65c92a0d63ecf1ad2672b29522b4e9a604f
Awedoba <severinasutani@gmail.com>
2023-01-24 13:17:38 -0800

    as

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: Severin
+ * Desc: Header file containing prototypes for all functions
+ *       used in the printf .
+ */
+
+#endif
